42a78a6f0b500829d72be8c7d1244413c36c4905
pranav3333 <kurankarpranav2299@gmail.com>
2025-10-07 01:18:32 +0530

    Introduce DTOs, entities, and JPA auditing setup for loan module development.

--- loans/src/main/java/com/hsbc/loans/LoansApplication.java
+++ loans/src/main/java/com/hsbc/loans/LoansApplication.java
@@ -2,7 +2,9 @@ package com.hsbc.loans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @SpringBootApplication
 public class LoansApplication {
 
---
+++ loans/src/main/java/com/hsbc/loans/audit/AuditAwareImpl.java
@@ -0,0 +1,4 @@
+package com.hsbc.loans.audit;
+
+public class AuditAwareImpl {
+}
